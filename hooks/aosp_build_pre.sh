79e1fa683c0117e5335cba01178bb7e725ab52db
laamalif <laamalif@pm.me>
2021-03-08 17:45:29 +0400

    added cloudflare-dns patch

--- hooks/aosp_build_pre.sh
+++ hooks/aosp_build_pre.sh
@@ -13,3 +13,9 @@ rm -rf "${community_patches_dir}"
 git clone https://github.com/rattlesnakeos/community_patches "${community_patches_dir}"
 patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00001-global-internet-permission-toggle.patch"
 patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00002-round-icon.patch"
+patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00004-use-cloudflare-dns.patch"
+
+## apply custom hosts file
+#custom_hosts_file="https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
+#echo "applying custom hosts file ${custom_hosts_file}"
+#retry wget -q -O "${AOSP_BUILD_DIR}/system/core/rootdir/etc/hosts" "${custom_hosts_file}"
